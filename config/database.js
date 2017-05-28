056979684dd3a90cf3f620451a8dfdcd03bb6e9b
folofolo <olarsson@live.com>
2017-05-28 13:32:30 +0200

    Skapat live databas, populerat den. Uppdaterat config. Enhetligt språk i alla views filer.

--- config/database.js
+++ config/database.js
@@ -1,5 +1,3 @@
 module.exports = {
-    // 'uri' : 'mongodb://admin:admin@ds137121.mlab.com:37121/yhjust16-car-rental'  //andreas test db
-    'uri': 'mongodb://test:test@ds029454.mlab.com:29454/testing' //olofs test db
-        //'uri' : 'mongodb://test:test@ds157390.mlab.com:57390/bookingtest'
+  'uri': 'mongodb://live:yesitis@ds155411.mlab.com:55411/live-carrental'
 }
